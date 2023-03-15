730fd2f8169ba186a7319fdcc0d296e449df52e0
coveetak88 <89083072+coveetak88@users.noreply.github.com>
2023-03-15 17:40:08 +0700

    Add files via upload

--- main.js
+++ main.js
@@ -10,9 +10,13 @@ const navSlide = () => {
 
     //Animate Links//
     navLinks.forEach((link, index) => {
-        link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7}s`;
-        console.log(index / 5);
-    })
+        if (link.style.animation) {
+            link.style.animation = "";
+        }
+        else {
+            link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
+        }
+    });
 }
 
 navSlide();
